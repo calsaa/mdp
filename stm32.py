2072f4c79b9c6307e9fad69493462d28c3a23eb6
shuyi <osy2020@gmail.com>
2025-10-02 15:14:48 +0800

    space changes

--- stm32.py
+++ stm32.py
@@ -7,6 +7,7 @@ BAUD = int(os.getenv("UART_BAUD", "115200"))
 def run_stm32_thread():
     ser = serial.Serial(UART, BAUD, timeout=0.1)
     first_command = True
+    ack_received = False
     while True:
         line = ser.readline().decode(errors="ignore").strip()
         if line or first_command:
@@ -17,27 +18,28 @@ def run_stm32_thread():
                 bus.trigger_camera.set()
             if "FIN" in line or first_command:
                 if not bus.to_stm32.empty():
-                   if first_command:
-                      first_command = False
-                   cmd = bus.to_stm32.get()
-                   cmd = cmd.strip()
-                   print(f"command from stm32 bus {cmd}")
-                   if "SNAP" not in cmd:
-                      if "FIN" not in cmd:
-                         print(f"sending {cmd} to stm32")
-                         ser.write((cmd + "\n").encode())
-                         android_cmd = bus.to_stm32.get()
-                         print(f"sending {android_cmd} to android")
-                         bus.to_android.put(android_cmd)
-#                         time.sleep(2)
-                   else:
-                      print("sending snap to bus")
-                      bus.snap_obstacle_id.put(cmd[4])
-                      bus.trigger_camera.set()
-                      first_command = True
-                  # time.sleep(3)
-            else :
-                   continue
+                    if first_command:
+                        first_command = False
+                    cmd = bus.to_stm32.get()
+                    cmd = cmd.strip()
+                    print(f"command from stm32 bus {cmd}")
+                    if "SNAP" not in cmd:
+                        #if "FIN" not in cmd:
+                        print(f"sending {cmd} to stm32")
+                        ser.write((cmd + "\n").encode())
+                        android_cmd = bus.to_stm32.get()
+                        print(f"sending {android_cmd} to android")
+                        bus.to_android.put(android_cmd)
+                        ack_received = False
+                        #time.sleep(2)
+                    else:
+                        print("sending snap to bus")
+                        bus.snap_obstacle_id.put(cmd[4])
+                        bus.trigger_camera.set()
+                        first_command = True
+                        time.sleep(3)
+            else:
+                continue
         if not bus.to_stm32.empty():
             cmd = bus.to_stm32.get()
             ser.write((cmd + "\n").encode())
